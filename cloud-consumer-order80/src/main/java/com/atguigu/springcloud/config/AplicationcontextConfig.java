95d0c2fe511ab06374dea666ebc9ddbbd1d6dda3
zqllz <740402045@qq.com>
2021-06-06 22:20:38 +0800

    【add】eureka

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/AplicationcontextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/AplicationcontextConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class AplicationcontextConfig {
 
     @Bean
-    //@LoadBalanced   //使用@LoadBalanced注解赋予RestTemplate负载均衡的能力
+    @LoadBalanced   //使用@LoadBalanced注解赋予RestTemplate负载均衡的能力
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
