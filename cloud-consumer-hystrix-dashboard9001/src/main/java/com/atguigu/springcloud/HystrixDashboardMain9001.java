b591779f60211ff335d505e43449f52ab61e8ab6
zqllz <740402045@qq.com>
2021-06-27 21:14:57 +0800

    【add】Hystrix-dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author yuluyang
+ * @date 2021/6/27 下午8:47
+ * @since 1.0.0-SNAPSHOT
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
