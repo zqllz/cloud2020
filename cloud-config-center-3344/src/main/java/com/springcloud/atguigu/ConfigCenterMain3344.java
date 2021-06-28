8723309fafb1fb477344a5c55a4663d2b7516e4c
zqllz <740402045@qq.com>
2021-06-29 00:06:29 +0800

    【add】spring-cloud-config-server

---
+++ cloud-config-center-3344/src/main/java/com/springcloud/atguigu/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.springcloud.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yuluyang
+ * @date 2021/6/28 下午11:01
+ * @since 1.0.0-SNAPSHOT
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
