bc20e2cca8a37e9198d6340c0b2595da4f37ea77
zqllz <740402045@qq.com>
2021-06-29 23:40:54 +0800

    【add】spring-cloud-config: 刷新配置

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yuluyang
+ * @date 2021/6/29 上午12:13
+ * @since 1.0.0-SNAPSHOT
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
