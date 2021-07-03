a6c3edc24c8363ec47ef04ed6f07aa264c77be4f
zqllz <740402045@qq.com>
2021-07-03 23:48:59 +0800

    【add】spring-cloud-alibaba-nacos: 服务提供者9001

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yuluyang
+ * @date 2021/7/3 下午3:34
+ * @since 1.0.0-SNAPSHOT
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
